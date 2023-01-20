68f50762599f724b0f8d6ee9511e91875ba6d5fe
oldakos <oldakos@volny.cz>
2023-01-20 15:12:38 +0100

    Add project files.

---
+++ DictionaryTree.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AnagramsTest
+{
+    internal class Node
+    {
+        internal void AddSuffix(IEnumerable<char> word)
+        {
+            Node currentNode = this;
+            Node nextNode = this;
+            foreach (char c in word)
+            {
+                bool foundNext = currentNode.TryGetChild(ref nextNode, c);
+                if (foundNext)
+                {
+                    currentNode = nextNode;
+                }
+                else
+                {
+                    nextNode = currentNode.CreateChild(c);
+                    currentNode = nextNode;
+                }
+            }
+            if (!currentNode.IsLeaf)
+            {
+                currentNode.MakeLeaf();
+            }
+        }
+
+        internal enum SearchResult
+        {
+            ABSENT,
+            FULL,
+            PARTIAL
+        }
+
+        /// <summary>
+        /// Check whether a path given by the word exists through this Node's children.
+        /// </summary>
+        /// <param name="word">The characters following this node's character.</param>
+        /// <returns>ABSENT if given word not traversable.<br/>FULL if traversable and final node is marked as end of a dictionary entry.<br/>PARTIAL if traversable but final node not end of any dictionary entry.</returns>
+        internal SearchResult CheckSuffix(IEnumerable<char> word)
+        {
+            //depends on all lowercase input
+
+            Node currentNode = this;
+            Node nextNode = this;
+            foreach (char c in word)
+            {
+                bool foundNext = currentNode.TryGetChild(ref nextNode, Char.ToLower(c));
+                if (foundNext)
+                {
+                    currentNode = nextNode;
+                }
+                else
+                {
+                    return SearchResult.ABSENT;
+                }
+            }
+            if (currentNode.IsLeaf)
+            {
+                return SearchResult.FULL;
+            }
+            else
+            {
+                return SearchResult.PARTIAL;
+            }
+        }        
+
+        private static readonly Node NULLNODE = new Node('\0');
+
+        readonly internal char Value;
+        readonly internal List<Node> Children;
+
+        internal Node(char value)
+        {
+            Value = value;
+            Children = new List<Node>();
+        }
+
+        internal Node CreateChild(char value)
+        {
+            Node n = new Node(value);
+            Children.Add(n);
+            return n;
+        }
+
+        internal bool TryGetChild(ref Node result, char value)
+        {
+            foreach (var child in Children)
+            {
+                if (child.Value == value)
+                {
+                    result = child;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        internal void MakeLeaf()
+        {
+            Children.Add(NULLNODE);
+        }
+
+        internal bool IsLeaf
+        {
+            get
+            {
+                return Children.Contains(NULLNODE);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AnagramsTest
+{
+    internal class Program
+    {
+        const string DICTIONARY_PATH = "words_alpha.txt";
+        const string ALPHA_CHARS = "abcdefghijklmnopqrstuvwxyz";
+
+        static void Main(string[] args)
+        {
+            Node root = new Node('\0');
+
+
+            DateTime start = DateTime.Now;
+
+            IEnumerable<string> lines = File.ReadLines(DICTIONARY_PATH);
+            foreach (var line in lines)
+            {
+                root.AddSuffix(line.Trim());
+            }
+
+            Console.WriteLine(DateTime.Now - start);
+
+            //string[] checks = { "Dog", "thi", "Mountains", "Swashbuckle", "Swashbuckleckleckle", "BBQ", "moun" };
+
+            //foreach (var word in checks)
+            //{
+            //    Console.WriteLine($"{word}? {root.CheckSuffix(word)}");
+            //}
+
+            while (true)
+            {
+                Console.WriteLine("Type any number of letters to use, or press Enter to use 9 random letters. Type STOP to stop.");
+                string? line = Console.ReadLine();
+                if (line == null) break;
+                if (line.ToUpper().Equals("STOP")) break;
+
+                List<char> chars = new List<char>();
+
+                if (line.Length == 0)
+                {
+                    AddRandomLetters(chars, 9);
+                }
+                else
+                {
+                    foreach (var c in line)
+                    {
+                        if (!char.IsWhiteSpace(c)) chars.Add(c);
+                    }
+                }
+
+
+                int bestScore;
+                List<string> bestWords;
+                Search(chars, root, out bestWords, out bestScore);
+
+                StringBuilder sb = new StringBuilder();
+                foreach (var c in chars)
+                {
+                    sb.Append(' ');
+                    sb.Append(c);
+                }
+
+                Console.WriteLine($"The used letters are:{sb.ToString()}");
+                Console.WriteLine($"The greatest length of an anagram is {bestScore}.");
+                Console.WriteLine("The following all have the same score:");
+                foreach (var word in bestWords)
+                {
+                    Console.WriteLine($"- {word}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void AddRandomLetters(List<char> list, int count)
+        {
+            Random r = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                int index = r.Next(ALPHA_CHARS.Length);
+                list.Add(ALPHA_CHARS[index]);
+            }
+        }
+
+        static void Search(List<char> chars, Node dictionaryPosition, out List<string> bestWords, out int score)
+        {
+            score = 0;
+            bestWords = new List<string>();
+
+            for (int i = 0; i < chars.Count; i++)
+            {
+                char selectedChar = chars[i];
+
+                Node subDictionaryPosition = dictionaryPosition;
+                bool childExists = dictionaryPosition.TryGetChild(ref subDictionaryPosition, selectedChar);
+                if (!childExists) continue;
+
+                List<char> subChars = new List<char>();
+                for (int j = 0; j < i; j++)
+                {
+                    subChars.Add(chars[j]);
+                }
+                for (int j = i + 1; j < chars.Count; j++)
+                {
+                    subChars.Add(chars[j]);
+                }
+
+                int subScore;
+                List<string> subBestWords;
+                Search(subChars, subDictionaryPosition, out subBestWords, out subScore);
+
+                int newScore = subScore + GetValue(selectedChar);
+
+                if (newScore > score)
+                {
+                    score = newScore;
+                    bestWords.Clear();
+                    foreach (var suffix in subBestWords)
+                    {
+                        bestWords.Add(selectedChar + suffix);
+                    }
+                }
+                else if (newScore == score)
+                {
+                    foreach (var suffix in subBestWords)
+                    {
+                        bestWords.Add(selectedChar + suffix);
+                    }
+                }
+            }
+
+            bool fail = (score == 0) && (dictionaryPosition.IsLeaf == false);
+            if (fail) score = int.MinValue;
+            if (score == 0 && !fail) bestWords.Add("");
+        }
+
+        static int GetValue(char c)
+        {
+            return 1;
+        }
+    }
+}
